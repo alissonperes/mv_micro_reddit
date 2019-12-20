af48e252be9c3819690f3efa9be0f7a987776920
Alisson Peres <alissonpres.dll@gmail.com>
2019-12-20 17:23:59 -0300

    Added user and post views for show and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
   def new; end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'posts/new'
-  get 'users/new'
+  resources 'users'
+  resources 'posts'
+  root 'posts#index'
+  # get 'comments/new'
+  # get 'posts/new'
+  # get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
