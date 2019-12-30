0bfc3f26198bdcd6991a4af8e8ad115bbb8cfa8a
Alisson Peres <alissonpres.dll@gmail.com>
2019-12-30 17:19:35 -0300

    Implemented user test and added password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
-  validates :name, presence: true, length: { maximum: 60 }
-  validates :email, presence: true,
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 70 }
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
   validates :username, presence: true, length: { minimum: 4, maximum: 60 },
                        uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
